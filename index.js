dca8b2889e1da31bbc18828e93b4aaeff97f663e
RogerDewald <daniel.r.dewald@gmail.com>
2024-04-25 00:42:13 -0500

    Added audio and final render

--- index.js
+++ index.js
@@ -2,10 +2,84 @@ const canvas = document.getElementById("drawing")
 const originalWidth = canvas.width
 const originalHeight = canvas.height
 const ctx = canvas.getContext("2d")
+const audioContext = new window.AudioContext();
+soundArr = [
+// Octave 5
+    523.25,  // C
+    554.37,  // C#
+    587.33,  // D
+    622.25,  // D#
+    659.25,  // E
+    698.46,  // F
+    739.99,  // F#
+    783.99,  // G
+    830.61,  // G#
+    880.00,  // A
+    932.33,  // A#
+    987.77,  // B
+
+    // Octave 6
+    1046.50, // C
+    1108.73, // C#
+    1174.66, // D
+    1244.51, // D#
+    1318.51, // E
+    1396.91, // F
+    1479.98, // F#
+    1567.98, // G
+    1661.22, // G#
+    1760.00, // A
+    1864.66, // A#
+    1975.53, // B
+
+    // Octave 7
+    2093.00, // C
+    2217.46, // C#
+    2349.32, // D
+    2489.02, // D#
+    2637.02, // E
+    2793.83, // F
+    2959.96, // F#
+    3135.96, // G
+    3322.44, // G#
+    3520.00, // A
+    3729.31, // A#
+    3951.07, // B
+
+    // Octave 8
+    4186.01, // C
+    4434.92, // C#
+    4698.64, // D
+    4978.03, // D#
+    5274.04, // E
+    5587.65, // F
+    5919.91, // F#
+    6271.93, // G
+    6644.88, // G#
+    7040.00, // A
+    7458.62, // A#
+    7902.13, // B
+
+    // Octave 9
+    8372.02, // C
+    8869.84, // C#
+    9397.27, // D
+    9956.06, // D#
+    10548.08,// E
+    11175.30,// F
+    11839.82,// F#
+    12543.85,// G
+    13289.75,// G#
+    14080.00,// A
+    14917.24,// A#
+    15804.26,// B
+]
+
+colorArr =["blue","black","red"]
 
 document.getElementById("bubble-button").addEventListener("click", function(){
     arr = makeArray()
-    bubbleSort(arr)
+    bubbleSort(arr, soundArr)
 })
 
 
@@ -13,10 +87,23 @@ function clearCanvas(){
     ctx.clearRect(0,0,originalWidth,originalHeight)
 }
 
-function makeBlock(arr){
+
+function makeBlock(arr, index){
     const blockLength = 10;
     const base = originalHeight
     for (let i = 0; i <arr.length; i++){
+        ctx.beginPath()
+        if (i == index ){
+            ctx.fillStyle = colorArr[0]
+        }
+        else {
+            ctx.fillStyle = colorArr[1]
+        }
+        ctx.moveTo(5 + i * blockLength,base)
+        ctx.lineTo(blockLength - 2+ i * blockLength,base)
+        ctx.lineTo(blockLength -2+ i * blockLength,base-1-5*arr[i])
+        ctx.lineTo(5 + i * blockLength,base-5*arr[i]-1)
+        ctx.fill()
 
         //ctx.beginPath()
         //ctx.rect(5 + i * blockLength,
@@ -25,13 +112,7 @@ function makeBlock(arr){
         //    base-5*arr[i]-1
         //)
         //ctx.fill()
-        ctx.beginPath()
-        ctx.moveTo(5 + i * blockLength,base)
-        ctx.lineTo(blockLength - 2+ i * blockLength,base)
-        ctx.lineTo(blockLength -2+ i * blockLength,base-1-5*arr[i])
-        ctx.lineTo(5 + i * blockLength,base-5*arr[i]-1)
         //ctx.lineTo(5 + i * blockLength,base)
-        ctx.fill()
 
     }
 }
@@ -51,8 +132,9 @@ function shuffleArray(array) {
   return array;
 }
 
-async function bubbleSort(arr){
-    makeBlock(arr)
+async function bubbleSort(arr, soundArr){
+    makeBlock(arr, 0)
+    let count = 0
     for (let i = 0; i <arr.length-1; i++){
         for (let j = 0; j<arr.length-1-i; j++){
             if(arr[j] > arr[j+1]){
@@ -60,13 +142,60 @@ async function bubbleSort(arr){
                 arr[j] = arr[j+1]
                 arr[j+1] = temp
                 clearCanvas()
-                makeBlock(arr)
-                await wait(50)
+                makeBlock(arr, j)
+                playSound(soundArr[j],10,0.01)
             }
+            await wait(10)
+            count += 1
         }
     }
+    makeLastBlock(arr)
 }
 
 function wait(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+function playSound(frequency, duration, volume) {
+    // Create an oscillator node
+    const oscillator = audioContext.createOscillator();
+
+    // Create a gain node
+    const gainNode = audioContext.createGain();
+
+    // Set the frequency
+    oscillator.frequency.setValueAtTime(frequency, audioContext.currentTime);
+
+    // Connect the oscillator to the gain node
+    oscillator.connect(gainNode);
+
+    // Connect the gain node to the audio output
+    gainNode.connect(audioContext.destination);
+
+    // Set the volume
+    gainNode.gain.setValueAtTime(volume, audioContext.currentTime);
+
+    // Start the oscillator
+    oscillator.start();
+
+    // Stop the oscillator after the specified duration
+    setTimeout(() => {
+        oscillator.stop();
+    }, duration);
+}
+
+async function makeLastBlock(arr){
+    const blockLength = 10;
+    const base = originalHeight
+    for (let i = 0; i <arr.length; i++){
+        ctx.beginPath()
+        ctx.fillStyle = colorArr[2]
+        ctx.moveTo(5 + i * blockLength,base)
+        ctx.lineTo(blockLength - 2+ i * blockLength,base)
+        ctx.lineTo(blockLength -2+ i * blockLength,base-1-5*arr[i])
+        ctx.lineTo(5 + i * blockLength,base-5*arr[i]-1)
+        ctx.fill()
+        playSound(soundArr[i],20,0.01)
+        await wait(20)
+    }
 }
