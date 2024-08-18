f9dc1aab8dfe99c2222c1b01a7fbb14ea6bfa03c
RogerDewald <daniel.r.dewald@gmail.com>
2024-08-17 22:18:48 -0500

    A lot of changes

--- src/index.js
+++ src/index.js
@@ -496,3 +496,67 @@ async function partition(arr, left, right) {
     makeColumn(arr, right, 3)
     return i;
 }
+
+async function heapSort(arr) {
+    render = true
+    var N = arr.length;
+
+    // Build heap (rearrange array)
+    for (var i = Math.floor(N / 2) - 1; i >= 0; i--)
+        await heapify(arr, N, i);
+    if (render == false){
+        return
+    }
+
+    // One by one extract an element from heap
+    for (var i = N - 1; i > 0; i--) {
+        // Move current root to end
+        var temp = arr[0];
+        arr[0] = arr[i];
+        arr[i] = temp;
+        makeColumn(arr, i, 2)
+        makeColumn(arr, 0, 4)
+        playSound(soundArr[i], 10, 0.01)
+        await wait(speedSelect())
+
+        // call max heapify on the reduced heap
+        await heapify(arr, i, 0);
+        if (render == false){
+            return
+        }
+    }
+}
+
+// To heapify a subtree rooted with node i which is
+// an index in arr[]. n is size of heap
+async function heapify(arr, N, i) {
+    if (render == false){
+        return
+    }
+    var largest = i; // Initialize largest as root
+    var l = 2 * i + 1; // left = 2*i + 1
+    var r = 2 * i + 2; // right = 2*i + 2
+
+    // If left child is larger than root
+    if (l < N && arr[l] > arr[largest])
+        largest = l;
+
+    // If right child is larger than largest so far
+    if (r < N && arr[r] > arr[largest])
+        largest = r;
+
+    // If largest is not root
+    if (largest != i) {
+        var swap = arr[i];
+        arr[i] = arr[largest];
+        arr[largest] = swap;
+        // Recursively heapify the affected sub-tree
+        await heapify(arr, N, largest);
+        if (render == false){
+            return
+        }
+    }
+    makeColumn(arr, i, 0)
+    makeColumn(arr, largest, 3)
+    await wait(speedSelect())
+}
